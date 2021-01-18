bff63a37a0b897ab22390c39384d3917a32b3f1a
Chris Bermeo <chrisberme@gmail.com>
2021-01-18 11:37:52 -0500

    Agregando base de datos remota (inicio)

--- ProyectoVacunas/app/src/main/java/com/example/proyectovacunas/Registrarse.kt
+++ ProyectoVacunas/app/src/main/java/com/example/proyectovacunas/Registrarse.kt
@@ -1,8 +1,10 @@
 package com.example.proyectovacunas
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
@@ -13,16 +15,44 @@ class Registrarse : AppCompatActivity() {
     private lateinit var btnRegistrarse: Button
     private lateinit var txtCorreo : EditText
     private lateinit var txtPassword : EditText
+    private lateinit var txtFecha : EditText
+    private lateinit var txtNombre : EditText
+    private lateinit var txtApellido : EditText
+    private lateinit var txtCedula : EditText
+
+    fun init(){
+        btnRegistrarse = findViewById(R.id.btnRegistrarse)
+        txtCorreo = findViewById(R.id.txtCorreo)
+        txtPassword = findViewById(R.id.txtPassword)
+        txtFecha = findViewById(R.id.txtFechaNacimiento)
+        txtNombre = findViewById(R.id.txtNombres)
+        txtApellido = findViewById(R.id.txtApellidos)
+        txtCedula = findViewById(R.id.txtCedula)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrarse)
-        btnRegistrarse = findViewById(R.id.btnRegistrarse)
-        txtCorreo = findViewById(R.id.txtCorreo)
-        txtPassword = findViewById(R.id.txtPassword)
-
+        init()
         setup()
     }
+
+    /*fun fecha(v:View){
+        showDatePickerDialog()
+    }
+
+
+    private fun showDatePickerDialog() {
+        val newFragment = DatePickerFragment.newInstance(DatePickerDialog.OnDateSetListener { _, year, month, day ->
+            // +1 because January is zero
+            val selectedDate = day.toString() + " / " + (month + 1) + " / " + year
+            txtFecha.setText(selectedDate)
+        })
+
+        newFragment.show(supportFragmentManager, "datePicker")
+    }*/
+
+
 
     private fun setup(){
         title ="Autenticación"
